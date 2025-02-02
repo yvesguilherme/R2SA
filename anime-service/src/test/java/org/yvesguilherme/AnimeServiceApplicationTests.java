4e581bcfc06a77a32bf308f6cc2b74300d57f25d
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2025-02-02 02:52:57 -0300

    🎉  Begin a project

---
+++ anime-service/src/test/java/org/yvesguilherme/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yvesguilherme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
