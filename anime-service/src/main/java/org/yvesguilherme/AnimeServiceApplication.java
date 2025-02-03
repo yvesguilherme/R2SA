5debd6c1decbc55c29aa402358429c9bef72e101
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2025-02-02 23:43:14 -0300

    ✨  Adding Spring Beans and ComponentScan

--- anime-service/src/main/java/org/yvesguilherme/AnimeServiceApplication.java
+++ anime-service/src/main/java/org/yvesguilherme/AnimeServiceApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"outside.yvesguilherne", "org.yvesguiherme"})
 public class AnimeServiceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(AnimeServiceApplication.class, args);
+
+//    var applicationContext = SpringApplication.run(AnimeServiceApplication.class, args);
+//    Arrays.stream(applicationContext.getBeanDefinitionNames())
+//            .forEach(System.out::println);
   }
 
 }
