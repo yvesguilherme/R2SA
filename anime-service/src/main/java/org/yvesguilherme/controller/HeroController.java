3cb73924cc36b1e7ea72bf09b7cc2efa9363de50
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2025-02-08 20:13:55 -0300

    ✨  Adding PathVariable

--- anime-service/src/main/java/org/yvesguilherme/controller/HeroController.java
+++ anime-service/src/main/java/org/yvesguilherme/controller/HeroController.java
@@ -1,9 +1,6 @@
 package org.yvesguilherme.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +22,14 @@ public class HeroController {
   @GetMapping("filterList")
   public List<String> listAllHeroesParamList(@RequestParam(defaultValue = "") List<String> names) {
     return HEROES.stream().filter(names::contains).toList();
+  }
+
+  @GetMapping("{name}")
+  public String findByName(@PathVariable String name) {
+    return HEROES
+            .stream()
+            .filter(hero -> hero.equalsIgnoreCase(name))
+            .findFirst()
+            .orElse("Hero not found!");
   }
 }
