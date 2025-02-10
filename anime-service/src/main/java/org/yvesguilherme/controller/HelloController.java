9dbe681e11ea8e275b19bb70593035c480ba83ad
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2025-02-09 22:24:59 -0300

    ✨  Adding PostMapping

--- anime-service/src/main/java/org/yvesguilherme/controller/HelloController.java
+++ anime-service/src/main/java/org/yvesguilherme/controller/HelloController.java
@@ -1,14 +1,22 @@
 package org.yvesguilherme.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 @RequestMapping(value = "greetings")
+@Slf4j
 public class HelloController {
-  @GetMapping("hi")
+  @GetMapping()
   public String hi() {
     return "OMAE WA MOU SHIND IRU";
+  }
+
+  @PostMapping
+  public Long save(@RequestBody String name) {
+    log.info("save '{}'", name);
+    return ThreadLocalRandom.current().nextLong(1, 1000);
   }
 }
