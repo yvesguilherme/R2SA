2b74be0546a98e1c476515121879a07b09c53c84
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2025-02-28 21:47:55 -0300

    ✨  Adding unit tests from ProducerHardCodedRepository

--- anime-service/src/test/java/org/yvesguilherme/repository/ProducerHardCodedRepositoryTest.java
+++ anime-service/src/test/java/org/yvesguilherme/repository/ProducerHardCodedRepositoryTest.java
@@ -12,6 +12,7 @@ import org.yvesguilherme.domain.Producer;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Use esta anotação quando você quer usar as funcionalidades do Spring.
@@ -92,5 +93,67 @@ class ProducerHardCodedRepositoryTest {
     Assertions
             .assertThat(producers)
             .contains(expectedProducer);
+  }
+
+  @Test
+  @DisplayName("save creates a producer")
+  void saveCreatesAProducerWhenSuccessful() {
+    BDDMockito.when(producerData.getProducers()).thenReturn(producerList);
+
+    var producerToSave = Producer.builder().id(99L).name("Mappa").createdAt(LocalDateTime.now()).build();
+
+    var producer = repository.save(producerToSave);
+
+    Assertions
+            .assertThat(producer)
+            .isEqualTo(producerToSave)
+            .hasNoNullFieldsOrProperties();
+
+    Optional<Producer> producerSavedOptional = repository.findById(producerToSave.getId());
+
+    Assertions
+            .assertThat(producerSavedOptional)
+            .isPresent()
+            .contains(producerToSave);
+  }
+
+  @Test
+  @DisplayName("delete removes a producer")
+  void deleteRemovesProducerWhenSuccessful() {
+    BDDMockito.when(producerData.getProducers()).thenReturn(producerList);
+
+    var producerToDelete = producerData.getProducers().getFirst();
+    repository.delete(producerToDelete);
+
+    var producers = repository.findAll();
+
+    Assertions
+            .assertThat(producers)
+            .isNotEmpty()
+            .doesNotContain(producerToDelete);
+  }
+
+  @Test
+  @DisplayName("update updates a producer")
+  void updateUpdatesAProducerWhenSuccessful() {
+    BDDMockito.when(producerData.getProducers()).thenReturn(producerList);
+    var producerToUpdate = this.producerList.getFirst();
+    producerToUpdate.setName("Aniplex");
+
+    repository.update(producerToUpdate);
+
+    Assertions
+            .assertThat(this.producerList)
+            .contains(producerToUpdate);
+
+    var producerUpdatedOptional = repository.findById(producerToUpdate.getId());
+
+    Assertions
+            .assertThat(producerUpdatedOptional)
+            .isPresent();
+
+    Assertions
+            .assertThat(producerUpdatedOptional.get().getName())
+            .isEqualTo(producerToUpdate.getName());
   }
 }
